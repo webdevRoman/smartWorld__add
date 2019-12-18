f57fd9d4f907e81daea9bfb411032d092df338cf
Roman <rgrabelnikov@yandex.ru>
2019-12-18 19:33:39 +0700

    Corrected task 2

--- 2/index.js
+++ 2/index.js
@@ -1,8 +1,12 @@
+Vue.use(VueMask.VueMaskPlugin)
+
 const app = new Vue({
   el: '#app',
   data: {
     formNumber: '',
+    currentNumber: '',
     formHolder: '',
+    currentHolder: '',
     formMonth: '',
     formYear: '',
     formCvv: '',
@@ -12,67 +16,11 @@ const app = new Vue({
   },
   methods: {
     cardNumber(inpNumber) {
-      let cardNumb = this.formNumber
+      let cardNumb = this.currentNumber
       while (cardNumb.length < 16) {
         cardNumb += '#'
       }
       return cardNumb.substr(4 * inpNumber, 4)
-    },
-    checkNumber(event) {
-      let oldVal = event.target.value
-      let newVal = oldVal.split('')
-      for (let i = 0; i < newVal.length; i++) {
-        if (newVal.length > 0 && newVal[i].match(/\D/))
-          newVal[i] = ''
-      }
-      if (newVal.length > 16)
-        newVal.length = 16
-      if (newVal[0] == 2) {
-        this.visa = false
-        this.mastercard = false
-        this.mir = true
-      } else if (newVal[0] == 5) {
-        this.visa = false
-        this.mastercard = true
-        this.mir = false
-      } else {
-        this.visa = true
-        this.mastercard = false
-        this.mir = false
-      }
-      newVal = newVal.join('')
-      this.formNumber = newVal
-      event.target.value = newVal
-    },
-    checkHolder(event) {
-      let oldVal = event.target.value
-      let newVal = oldVal.split('')
-      for (let i = 0; i < newVal.length; i++) {
-        if (newVal.length > 0 && newVal[i].match(/[^A-Za-z ]/))
-          newVal[i] = ''
-      }
-      if (newVal.length > 30)
-        newVal.length = 30
-      newVal = newVal.join('').toUpperCase()
-      this.formHolder = newVal
-      event.target.value = newVal
-      const cardInput = document.querySelector('.card-holder')
-      cardInput.value = newVal
-    },
-    checkMonth(event) {
-      let oldVal = event.target.value
-      let newVal = oldVal.split('')
-      for (let i = 0; i < newVal.length; i++) {
-        if (newVal.length > 0 && newVal[i].match(/\D/))
-          newVal[i] = ''
-      }
-      if (newVal.length > 2)
-        newVal.length = 2
-      newVal = newVal.join('')
-      this.formMonth = newVal
-      event.target.value = newVal
-      const cardInput = document.querySelector('.card-month')
-      cardInput.value = newVal
     },
     correctMonth(event) {
       let oldVal = Number(event.target.value)
@@ -92,21 +40,6 @@ const app = new Vue({
       event.target.value = oldVal
       cardInput.value = oldVal
     },
-    checkYear(event) {
-      let oldVal = event.target.value
-      let newVal = oldVal.split('')
-      for (let i = 0; i < newVal.length; i++) {
-        if (newVal.length > 0 && newVal[i].match(/\D/))
-          newVal[i] = ''
-      }
-      if (newVal.length > 2)
-        newVal.length = 2
-      newVal = newVal.join('')
-      this.formYear = newVal
-      event.target.value = newVal
-      const cardInput = document.querySelector('.card-year')
-      cardInput.value = newVal
-    },
     correctYear(event) {
       let oldVal = Number(event.target.value)
       const cardInput = document.querySelector('.card-year')
@@ -120,21 +53,6 @@ const app = new Vue({
       event.target.value = oldVal
       this.formYear = oldVal
       cardInput.value = oldVal
-    },
-    checkCvv(event) {
-      let oldVal = event.target.value
-      let newVal = oldVal.split('')
-      for (let i = 0; i < newVal.length; i++) {
-        if (newVal.length > 0 && newVal[i].match(/\D/))
-          newVal[i] = ''
-      }
-      if (newVal.length > 3)
-        newVal.length = 3
-      newVal = newVal.join('')
-      this.formCvv = newVal
-      event.target.value = newVal
-      const cardInput = document.querySelector('.card-cvv__input')
-      cardInput.value = newVal
     },
     correctCvv(event) {
       let oldVal = Number(event.target.value)
@@ -163,6 +81,34 @@ const app = new Vue({
         alert('Заполнены не все поля формы')
       else
         alert('Форма успешно отправлена')
+    }
+  },
+  watch: {
+    formNumber(value) {
+      let cardVal = value.split('')
+      if (cardVal[0] == 2) {
+        this.visa = false
+        this.mastercard = false
+        this.mir = true
+      } else if (cardVal[0] == 5) {
+        this.visa = false
+        this.mastercard = true
+        this.mir = false
+      } else {
+        this.visa = true
+        this.mastercard = false
+        this.mir = false
+      }
+      this.currentNumber = cardVal.filter(symbol => symbol != ' ').join('')
+    },
+    formHolder(value) {
+      this.formHolder = value.toUpperCase()
+      const cardInput = document.querySelector('.card-holder')
+      cardInput.value = this.formHolder
+    },
+    formCvv(value) {
+      const cardInput = document.querySelector('.card-cvv__input')
+      cardInput.value = value
     }
   }
 })
